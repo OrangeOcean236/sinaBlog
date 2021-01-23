2614d5c1d07f52d4a612aac1e995c69e9fee1450
OrangeOcean236 <3353498847@qq.com>
2021-01-23 16:39:44 +0800

    用户信息借口配置成功

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 实现用户提交信息，注册事项
+// Response   响应
+// Request    请求
+
+router.post('/addUser', (req, res, next) => {
+
+  // console.log(req.body.userName);
+  // console.log(req.body.password);
+  // console.log(req.body.passwordC);
+  // 用户填写的表单信息可以通过req.body获取到
+
+  // res.send('点击注册了')
+  // console.log('点击注册了')
+})
+
+
 module.exports = router;
